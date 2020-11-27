1efeff21e1e2e6a07649e0bca0cf5483c7810bdf
FOkoro <33636439+FOkoro@users.noreply.github.com>
2020-11-27 08:15:44 -0600

    Modified App.tsx by adding some test timages and including the imports and a slider.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Register from './components/Register/Register';
 import Email from './components/Register/Email';
 import Profile from './components/Profile/Profile';
 import Cart from './components/Cart/Cart';
+import Slider from './components/Slider/Slider.js';
 
 import { _firebase, _item, _models } from './scripts/Models';
 import firebase from 'firebase/app';
@@ -15,7 +16,15 @@ import Firestore from './scripts/Firestore';
 import Item from './scripts/Item';
 import { useState } from 'react';
 
-const config:_firebase = {
+//Testing 
+const images = [
+ 'https://img.wallpapersafari.com/desktop/1920/1080/98/35/dW6m0X.jpg',
+ 'https://img.wallpapersafari.com/desktop/1920/1080/85/15/lpCj75.jpg',
+ 'https://img.wallpapersafari.com/desktop/1920/1080/75/54/ZBLizC.jpg',
+ 'https://img.wallpapersafari.com/desktop/1920/1080/23/36/per8iz.jpg'
+]
+
+const config: _firebase = {
   apiKey: "AIzaSyDRJbMO8p_kOJY6AH6VPRdMQYe1xjwo3zg",
   authDomain: "merkfabz.firebaseapp.com",
   databaseURL: "https://merkfabz.firebaseio.com",
@@ -26,15 +35,15 @@ const config:_firebase = {
   measurementId: "G-NTVWNWY41C"
 };
 
-const app:firebase.app.App = firebase.initializeApp(config);
+const app: firebase.app.App = firebase.initializeApp(config);
 
-async function getItems(setItems:any, store:Firestore) {
+async function getItems(setItems: any, store: Firestore) {
 
-  let items:Item[] = [];
+  let items: Item[] = [];
 
-  let db_items:_item[] = await store.getAllDocs("ITEMS");
+  let db_items: _item[] = await store.getAllDocs("ITEMS");
 
-  for(let i = 0; i < db_items.length; i++)
+  for (let i = 0; i < db_items.length; i++)
     items.push(new Item(db_items[i]));
 
   setItems(items);
@@ -42,7 +51,7 @@ async function getItems(setItems:any, store:Firestore) {
 
 export default function App() {
 
-  let curr_items:Item[] = [];
+  let curr_items: Item[] = [];
 
   const [items, setItems] = useState(curr_items);
   const [store, setStore] = useState(new Firestore(app.firestore()));
@@ -50,13 +59,21 @@ export default function App() {
 
   //getItems(setItems, store);
 
-  let models:_models = {
+  let models: _models = {
     auth: auth,
     store: store
   }
+  // <SideBar />
+  //   <Header {...models} />
+  // <Cart {...models} />
+  //<Login {...models} />
+  // <Register {...models} />
+  // <Email {...models} />
+  //<Profile {...models} />
 
   return (
     <>
+      <Slider slides={images} />
       <SideBar />
       <Header {...models} />
       <Cart {...models} />
